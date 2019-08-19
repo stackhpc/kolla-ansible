c486649d29fc19a50345355e024057d3dc845041
Doug Szumski <doug@stackhpc.com>
2019-08-19 15:52:46 +0100

    Factor out nova-cell role
    Splitting this role out from the Nova role allows a more granular
    approach to deploying and configuring Nova services. It paves the
    way for having a role which can be used to deploy multiple Nova cells.
    
    This patch does not support using a single cell controller for managing
    more than one cell. Support for sharing a cell controller will be added
    in a future patch.
    
    This patch should be backwards compatible and is tested by existing CI
    jobs. A Future patch will add a multi-cell test.
    
    Partially Implements: blueprint support-nova-cells
    Co-Authored-By: Mark Goddard <mark@stackhpc.com>
    Change-Id: I810aad7d49db3f5a7fd9a2f0f746fd912fe03917

---
+++ ansible/roles/nova-cell/filter_plugins/extract_cell.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from jinja2.exceptions import TemplateRuntimeError
+import re
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'extract_cell': self._extract_cell,
+        }
+
+    def _extract_cell(self, list_cells_cli_output, cell_name):
+        """Extract cell settings from nova_manage CLI
+
+        This filter tries to extract the cell settings for the specified cell
+        from the output of the command:
+        nova-manage cell_v2 list_cells --verbose
+        If the cell is not registered, nothing is returned.
+
+        An example line from this command for a cell with no name looks like this:
+
+        |  | 68a3f49e-27ec-422f-9e2e-2a4e5dc8291b | rabbit://openstack:password@1.2.3.4:5672 | mysql+pymysql://nova:password@1.2.3.4:3306/nova |  False   |  # noqa
+
+        And for a cell with a name:
+
+        | cell1 | 68a3f49e-27ec-422f-9e2e-2a4e5dc8291b | rabbit://openstack:password@1.2.3.4:5672 | mysql+pymysql://nova:password@1.2.3.4:3306/nova |  False   |  # noqa
+
+        """
+        # NOTE(priteau): regexp doesn't support passwords containing spaces
+        p = re.compile(
+            r'\| +(?P<cell_name>[^ ]+)? +'
+            r'\| +(?!00000000-0000-0000-0000-000000000000)'
+            r'(?P<cell_uuid>[0-9a-f\-]+) +'
+            r'\| +(?P<cell_message_queue>[^ ]+) +'
+            r'\| +(?P<cell_database>[^ ]+) +'
+            r'\| +(?P<cell_disabled>[^ ]+) +'
+            r'\|$')
+        cells = []
+        for line in list_cells_cli_output['stdout_lines']:
+            match = p.match(line)
+            if match:
+                # If there is no cell name, we get None in the cell_name match
+                # group. Use an empty string to match the default cell.
+                match_cell_name = match.group('cell_name') or ""
+                if match_cell_name == cell_name:
+                    cells.append(match.groupdict())
+        if len(cells) > 1:
+            raise TemplateRuntimeError(
+                "Cell: {} has duplicates. "
+                "Manual cleanup required.".format(cell_name))
+        return cells[0] if cells else None
