80db0e1d995c047a47de2729506c415898be85b3
Will Szumski <will@stackhpc.com>
2023-03-17 09:46:20 +0000

    Adds the neutron_service_limit option
    This can be used to perform neutron upgrades in two steps:
    
    - First upgrade neutron server
    - Then upgrade the agents
    
    This can help minimise L3 downtime.
    
    Co-authored-by: Tyler Christie <tyler@stackhpc.com>
    
    Change-Id: Ied685bc7eb71d232b4532e2c598458da7b4c6aa4

--- tests/upgrade.sh
+++ tests/upgrade.sh
@@ -14,7 +14,16 @@ function upgrade {
 
     kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks &> /tmp/logs/ansible/upgrade-prechecks
     kolla-ansible -i ${RAW_INVENTORY} -vvv pull &> /tmp/logs/ansible/pull-upgrade
-    kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade &> /tmp/logs/ansible/upgrade
+
+    if [[ $SCENARIO == "ovs_split_upgrade" ]]; then
+        kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade --skip-tags neutron,openvswitch &> /tmp/logs/ansible/upgrade
+        kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade --tags neutron -e neutron_service_limit=neutron-server &> /tmp/logs/ansible/upgrade-neutron-server
+        # NOTE(wszumski): We are skipping the process of draining and upgrading each network node separately for simplicity. This could be added later.
+        kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade --limit network --tags neutron,openvswitch -e neutron_service_limit='neutron-openvswitch-agent,neutron-dhcp-agent,neutron-l3-agent,neutron-metadata-agent,ironic-neutron-agent' &> /tmp/logs/ansible/upgrade-neutron-network-nodes
+        kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade --limit compute --tags neutron,openvswitch &> /tmp/logs/ansible/upgrade-neutron-computes
+    else
+        kolla-ansible -i ${RAW_INVENTORY} -vvv upgrade &> /tmp/logs/ansible/upgrade
+    fi
 
     # NOTE(yoctozepto): These actions remove the leftovers of the admin port.
     # TODO(yoctozepto): Remove after Zed.
